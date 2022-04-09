d50e3b1608bd0c29d2709bbba49c73c890f0dffa
Błażej <blazejkob1342@gmail.com>
2022-04-09 20:00:20 +0200

    Final v1.0

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        val btnStart : Button = findViewById(R.id.btnStart)
+        val etName : EditText = findViewById(R.id.etName)
+        btnStart.setOnClickListener(){
+
+            if(etName.text.isEmpty()){
+                Toast.makeText(this,
+                    "Please enter your name", Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, QuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, etName.text.toString())
+                startActivity(intent)
+                finish()
+            }
+        }
+
     }
 }
